cf8531df5d34cb678df4674a885bb319da314c74
LukasXavier <lucasxavier@email.arizona.edu>
2022-09-14 16:48:46 -0700

    commit test

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/*", (_, res) => {
-    res.send("Dockerfile worked v2");
+    res.send("Dockerfile worked v0.3");
 });
 
 app.listen(80, () => { console.log("server started"); });
